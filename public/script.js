3a403d564a0fef3acc5d37953eca42c62509b1d6
Cronwel Noel Irias <noelirias@gmail.com>
2018-02-04 23:05:57 -0500

    addItem method created

--- public/script.js
+++ public/script.js
@@ -1,6 +1,18 @@
 new Vue({
   el: '#app',
   data: {
-    total: 0
+    total: 0,
+    items: [
+      { title: 'Item 1' },
+      { title: 'Item 2' },
+      { title: 'Item 3' },
+      { title: 'Item 4' }
+    ],
+    cart: []
+  },
+  methods: {
+    addItem: function() {
+      this.total += 9.99;
+    }
   }
 });
